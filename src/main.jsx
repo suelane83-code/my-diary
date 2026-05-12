6f6b6cfc26590b378a2880b7340ddde9b1b52476
MyName <myemail@gmail.com>
2026-05-12 08:51:37 +0800

    Add tailwind config files

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // ⚠️ 就是漏了这一句魔法代码！必须加上！
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
